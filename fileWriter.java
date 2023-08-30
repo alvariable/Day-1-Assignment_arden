4905aa87732acb812c9dc5a59c58aee6682f19d8
alvariable <thepottermoreofarden@gmail.com>
2023-08-30 12:06:35 -0700

    added new method
    added a countCharacters method in the FileWriter class

--- fileWriter.java
+++ fileWriter.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class fileWriter {
@@ -12,4 +15,15 @@ public class fileWriter {
         pw.print("This is what I'm printing to the file. Not sure what else to write");
         pw.close();
     }
+
+    public int countCharacters(String fileName) throws IOException {
+        int count = 0;
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        while (br.ready()) {
+            count++;
+        }
+        br.close();
+        return count;
+    }
+
 }
